ebc65e97faf15f2f047f57bea65c6d3043790830
tranduchoangnam <20febnam@gmail.com>
2024-05-11 13:40:54 +0700

    UI auth

--- src/layouts/auth.jsx
+++ src/layouts/auth.jsx
@@ -33,7 +33,7 @@ export function Auth() {
   ];
 
   return (
-    <div className="relative min-h-screen w-full">
+    <div className="relative min-h-screen w-full bg-[#F5F6FA] font-['Inter']">
       <Routes>
         {routes.map(
           ({ layout, pages }) =>
